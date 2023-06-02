4643357ec8cee2ed79e3743f058db7bfd6917c50
Carlos Kamisato <carloskamisato.ktd@orakle.cl>
2023-06-01 21:10:45 -0400

    junit

---
+++ src/main/java/cl/awakelab/junitapp/App.java
@@ -0,0 +1,10 @@
+package cl.awakelab.junitapp;
+
+public class App {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hola Mundo Integración WebHook 4");
+  }
+
+}
